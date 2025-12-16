a0ee1657e7799f4de50cbf6e6e7f71fb7ce977d5
Breuninger Matthias (ETAS-ICA/XPC-Fe3) <matthias.breuninger@etas.com>
2025-12-16 10:33:21 +0100

    update: Make header-only

--- cpp/src/srpcpp/common.hpp
+++ cpp/src/srpcpp/common.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <map>
+#include <regex>
 
 #include <libyang-cpp/Context.hpp>
 #include <libyang-cpp/DataNode.hpp>
@@ -17,6 +18,18 @@ namespace srpc
 {
 
 /**
+ * @brief Escape special characters in the incoming string so that it can be used in a regular expression.
+ *
+ * @param s Incoming string
+ * @return std::string Escaped string
+ */
+static std::string EscapeForRegex(const std::string &s)
+{
+    static const std::regex meta_characters{R"([-[\]{}()*+?.\^$|\s])"};
+    return std::regex_replace(s, meta_characters, R"(\$&)");
+}
+
+/**
  * @brief Extracts the key from the list XPath.
  *
  * @param list List name.
@@ -24,7 +37,30 @@ namespace srpc
  * @param xpath XPath of the list.
  * @return std::string Key value.
  */
-const std::string extractListKeyFromXPath(const std::string &list, const std::string &key, const std::string &xpath);
+static const std::string extractListKeyFromXPath(const std::string &list, const std::string &key, const std::string &xpath)
+{
+    std::string value;
+
+    std::stringstream ss;
+
+    ss << EscapeForRegex(list) << "\\[" << key << "='([^']*)'\\]";
+
+    const auto &xpath_expr = ss.str();
+
+    std::regex re(xpath_expr);
+    std::smatch xpath_match;
+
+    if (std::regex_search(xpath, xpath_match, re))
+    {
+        value = xpath_match[1];
+    }
+    else
+    {
+        throw std::runtime_error("Failed to extract key from XPath.");
+    }
+
+    return value;
+}
 
 /**
  * @brief Extracts all the keys from the list XPath by list name.
@@ -33,7 +69,52 @@ const std::string extractListKeyFromXPath(const std::string &list, const std::st
  * @param xpath XPath of the list.
  * @return std::unordered_map<std::string, std::string> Key values.
  */
-std::unordered_map<std::string, std::string> extractListKeysFromXpath(const std::string& list, const std::string& xpath);
+static std::unordered_map<std::string, std::string> extractListKeysFromXpath(const std::string& list, const std::string& xpath)
+{
+    std::unordered_map<std::string, std::string> keys_map;
+
+    int starting_point = xpath.find(list + "[");
+
+    std::string chunk(xpath.begin() + starting_point, xpath.end());
+
+    int ending_point = chunk.find("]/");
+
+    if(ending_point == std::string::npos){
+        //this means it can be last
+        ending_point = chunk.find_last_of(']');
+    }
+
+    chunk.erase(chunk.begin() + ending_point + 1, chunk.end());
+
+    int begin = chunk.find('[');
+    int end = chunk.find(']');
+    // list is found, continue
+
+    while (begin != std::string::npos || end != std::string::npos) {
+
+        std::string mapstr(++chunk.begin() + begin, chunk.begin() + end);
+        chunk.erase(begin, ++end - begin);
+
+        // now split it by '=' in key value
+
+        int eq_pos = mapstr.find('=');
+
+        if (eq_pos == std::string::npos) {
+            throw std::runtime_error("Failed to parse '=' sympol");
+        }
+
+        std::string key(mapstr.begin(), mapstr.begin() + eq_pos);
+        // value shrink by one place begin to end to eliminate ' '
+        std::string value(mapstr.begin() + eq_pos + 2, --mapstr.end());
+
+        keys_map.insert(std::make_pair(key, value));
+
+        begin = chunk.find('[');
+        end = chunk.find(']');
+    }
+
+    return keys_map;
+}
 
 /**
  * @brief Get meta value.
@@ -42,7 +123,17 @@ std::unordered_map<std::string, std::string> extractListKeysFromXpath(const std:
  * @param name Meta name.
  * @return std::string Meta value.
  */
-const std::string getMetaValue(const ly::MetaCollection &meta, const std::string &name);
+static const std::string getMetaValue(const ly::MetaCollection &meta, const std::string &name)
+{
+    for (const auto &m : meta)
+    {
+        if (m.name() == name)
+        {
+            return m.valueStr();
+        }
+    }
+    throw std::runtime_error("Failed to get meta value.");
+}
 
 /**
  * @brief Convert meta values list to a hash.
@@ -50,7 +141,17 @@ const std::string getMetaValue(const ly::MetaCollection &meta, const std::string
  * @param meta Meta collection object.
  * @return std::map<std::string, std::string> Meta values hash.
  */
-std::map<std::string, std::string> getMetaValuesHash(const ly::MetaCollection meta);
+static std::map<std::string, std::string> getMetaValuesHash(const ly::MetaCollection meta)
+{
+    std::map<std::string, std::string> hash;
+
+    for (const auto &m : meta)
+    {
+        hash[m.name()] = m.valueStr();
+    }
+
+    return hash;
+}
 
 /**
  * Register all operational plugin subscriptions.
@@ -61,7 +162,7 @@ std::map<std::string, std::string> getMetaValuesHash(const ly::MetaCollection me
  *
  */
 template <PluginContext PluginContextType>
-void registerOperationalSubscriptions(sysrepo::Session sess, PluginContextType &ctx,
+static void registerOperationalSubscriptions(sysrepo::Session sess, PluginContextType &ctx,
                                       std::unique_ptr<srpc::IModule<PluginContextType>> &mod)
 {
     const auto oper_callbacks = mod->getOperationalCallbacks();
@@ -91,7 +192,7 @@ void registerOperationalSubscriptions(sysrepo::Session sess, PluginContextType &
  *
  */
 template <PluginContext PluginContextType>
-void registerModuleChangeSubscriptions(sysrepo::Session sess, PluginContextType &ctx,
+static void registerModuleChangeSubscriptions(sysrepo::Session sess, PluginContextType &ctx,
                                        std::unique_ptr<srpc::IModule<PluginContextType>> &mod)
 {
     const auto change_callbacks = mod->getModuleChangeCallbacks();
@@ -121,7 +222,7 @@ void registerModuleChangeSubscriptions(sysrepo::Session sess, PluginContextType 
  *
  */
 template <PluginContext PluginContextType>
-void registerRpcSubscriptions(sysrepo::Session sess, PluginContextType &ctx,
+static void registerRpcSubscriptions(sysrepo::Session sess, PluginContextType &ctx,
                               std::unique_ptr<srpc::IModule<PluginContextType>> &mod)
 {
     const auto rpc_callbacks = mod->getRpcCallbacks();
